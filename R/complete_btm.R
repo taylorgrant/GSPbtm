542e10845b86e1dda2376dcb01eb29bd4682edc0
taylorgrant <taylorgrant38@gmail.com>
2022-11-08 16:29:33 -0800

    allowing model choice

--- R/complete_btm.R
+++ R/complete_btm.R
@@ -11,19 +11,19 @@
 #' \dontrun{
 #' complete_btm(data, min_topics, max_topics)
 #' }
-complete_btm <- function(data, min_topics, max_topics) {
+complete_btm <- function(data, min_topics, max_topics, modeltype) {
 
   # 1. POS tagging by model type
   cat("Annotating the texts provided...\n")
-  NAV_reviews <- btm_annotate(data, "NAV")
+  Reviews <- btm_annotate(data, modeltype)
   # 2. model across n topics
   cat("Estimating topic models...\n")
   n_topics <- as.integer(min_topics):as.integer(max_topics)
   testing_models <- n_topics |>
-    purrr::map(btm_topicmodel, data = NAV_reviews) |>
+    purrr::map(btm_topicmodel, data = Reviews) |>
     purrr::set_names(nm = paste0("topicsize_", n_topics))
   # 3a. convert data to DTM for coherence scoring
-  dtm <- btm_buildDTM(NAV_reviews)
+  dtm <- btm_buildDTM(Reviews)
   # 3b. coherence scoring
   cat("Determining coherence scores...\n")
   coherence <- purrr::map_dfr(testing_models, btm_coherence, DTM = dtm) |>
@@ -31,11 +31,11 @@ complete_btm <- function(data, min_topics, max_topics) {
                         names_to = "Topic", values_to = "Value")
   # 4. pca coordinates and top terms
   cat("Extracting topic coordinates and top terms per topic...\n")
-  pca_coords <- purrr::map(testing_models, btm_pca_coords, data = NAV_reviews)
+  pca_coords <- purrr::map(testing_models, btm_pca_coords, data = Reviews)
   # 5. extract the topic with the best coherence
   best_topic <- btm_best_topic(pca_coords, coherence)
   # 6. Probabilistic mapping of topics back to reviews
-  modeled_topic <- btm_mapping(testing_models, NAV_reviews, coherence, best_topic)
+  modeled_topic <- btm_mapping(testing_models, Reviews, coherence, best_topic)
 
   cat("Putting everything together...\n")
   topic_names <- modeled_topic$topic_names |>
